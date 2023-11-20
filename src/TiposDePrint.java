1d68a8a6861d34cbb7afde1c0b34d5682d776d99
LeonardoSantosBR <leosantosmelo07@gmail.com>
2023-11-20 15:18:44 -0300

    fix: tipos de print

---
+++ src/TiposDePrint.java
@@ -0,0 +1,7 @@
+public class TiposDePrint {
+    public static void main(String[] args){
+        System.out.print("print , não quebra a linha.\n");
+        System.out.println("print, quebra a linha");
+        System.out.printf("%s - %d", "print, com padrões", 2023);
+    }
+}
