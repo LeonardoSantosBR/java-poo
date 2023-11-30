41f7ca2321793f112cdb10ef92c73b6646c96d63
LeonardoSantosBR <leosantosmelo07@gmail.com>
2023-11-30 09:49:59 -0300

    fix: loops

--- src/Loops.java
+++ src/Loops.java
@@ -1,15 +1,24 @@
+import java.util.Scanner;
+
 public class Loops {
     public static void main(String[] args){
         int it = 0;
+        Scanner scan = new Scanner(System.in);
 
         for (it = 0; it < 5; it++){
             System.out.println(it);
         }
 
-        while(it <= 5){
-            it++;
-            System.out.println(it);
+        System.out.println("----------------------------------------");
+
+        int count = 0;
+        int max = scan.nextInt();
+        while(count <= max){
+            count++;
+            System.out.println(count);
         }
+
+        System.out.println("----------------------------------------");
 
         do {
             it++;
