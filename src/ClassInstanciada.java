25cfe030de8cd1954e47201e1eea70c825ec55ec
LeonardoSantosBR <leosantosmelo07@gmail.com>
2024-01-03 09:34:22 -0300

    feat: instanciando classe.

---
+++ src/ClassInstanciada.java
@@ -0,0 +1,7 @@
+public class ClassInstanciada {
+    public static void main(String[] args){
+        ClassPoo c1 = new ClassPoo(1,"leosantosmelo07@gmail.com");
+
+        System.out.println(c1);
+    }
+}
